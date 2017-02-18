ae0d2ea00e7a7f7344850642e988191d6ecacbfe
ku00 <skentaro36@gmail.com>
2017-02-18 23:33:27 +0900

    他の活性化関数も描画する
    - シグモイド関数とReLU関数も描画する
    - 入力を 0.1 刻みにしてより書籍のグラフに近づけるようにした

--- src/ActivationFunction.hs
+++ src/ActivationFunction.hs
@@ -1,8 +1,16 @@
 module ActivationFunction
     ( step'
+    , sigmoid
+    , relu
     ) where
 
 import Numeric.LinearAlgebra
 
 step' :: Vector R -> Vector R
 step' = cmap (\x -> if x>0 then 1 else 0)
+
+sigmoid :: Vector R -> Vector R
+sigmoid = cmap (\x -> 1 / (1 + exp(-x)))
+
+relu :: Vector R -> Vector R
+relu = cmap (max 0)
---
+++ src/PlotActivationFunction.hs
@@ -0,0 +1,18 @@
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Diagrams(toFile)
+
+import ActivationFunction
+import Numeric.LinearAlgebra.Data
+
+main = toFile def "activation_function.svg" $ do
+    layout_title .= "activation function"
+    let input = vector [(-5),(-4.9)..5]
+    plot (line "step" [stepSignals input])
+    plot (line "sigmoid" [sigmoidSignals input])
+    plot (line "relu" [reluSignals input])
+
+stepSignals xs = zipWith (,) (toList xs) (toList $ step' xs)
+
+sigmoidSignals xs = zipWith (,) (toList xs) (toList $ sigmoid xs)
+
+reluSignals xs = zipWith (,) (toList xs) (toList $ relu xs)
