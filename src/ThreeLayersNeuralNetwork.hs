a5fe3d5f6bc0dc20dd6c9a60995277bc0787175a
ku00 <skentaro36@gmail.com>
2017-02-27 13:05:16 +0900

    Double で指定しなくても Matrix R があったのだった

--- src/ThreeLayersNeuralNetwork.hs
+++ src/ThreeLayersNeuralNetwork.hs
@@ -2,17 +2,17 @@ import ActivationFunction
 import Numeric.LinearAlgebra
 
 main = do
-    let x = (1><2) [1.0, 0.5::Double]
+    let x = (1><2) [1.0, 0.5] :: Matrix R
         y = forward x
     print y
 
 forward x =
-    let w1 = (2><3) [0.1, 0.3, 0.5, 0.2, 0.4, 0.6::Double]
-        w2 = (3><2) [0.1, 0.4, 0.2, 0.5, 0.3, 0.6::Double]
-        w3 = (2><2) [0.1, 0.3, 0.2, 0.4::Double]
-        b1 = (1><3) [0.1, 0.2, 0.3::Double]
-        b2 = (1><2) [0.1, 0.2::Double]
-        b3 = (1><2) [0.1, 0,2::Double]
+    let w1 = (2><3) [0.1, 0.3, 0.5, 0.2, 0.4, 0.6] :: Matrix R
+        w2 = (3><2) [0.1, 0.4, 0.2, 0.5, 0.3, 0.6] :: Matrix R
+        w3 = (2><2) [0.1, 0.3, 0.2, 0.4] :: Matrix R
+        b1 = (1><3) [0.1, 0.2, 0.3] :: Matrix R
+        b2 = (1><2) [0.1, 0.2] :: Matrix R
+        b3 = (1><2) [0.1, 0,2] :: Matrix R
         a1 = (x <> w1) + b1
         z1 = sigmoid a1
         a2 = (z1 <> w2) + b2
