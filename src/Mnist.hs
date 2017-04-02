a1d11d5a13a34e4c0368e8d91cf1aa8c81304268
ku00 <skentaro36@gmail.com>
2017-04-02 17:28:25 +0900

    convert matrix train images
    try convert 'train-images-idx3-ubyte.gz'

--- src/Mnist.hs
+++ src/Mnist.hs
@@ -1,12 +1,14 @@
 module Mnist
     ( initMnist
+    , loadImg
     ) where
 
 import Numeric.LinearAlgebra
 import Network.HTTP.Simple ( parseRequest, httpLBS, getResponseBody )
 import System.Directory ( doesFileExist )
-import Control.Monad ( unless )
-import qualified Data.ByteString.Lazy as B ( writeFile )
+import Control.Monad ( unless, mapM_ )
+import qualified Data.ByteString.Lazy as BL (unpack, drop, writeFile, readFile)
+import qualified Codec.Compression.GZip as GZ ( decompress )
 
 type DataSet = (Matrix R, Matrix R)
 
@@ -18,17 +20,21 @@ keyFiles = [
             ("test_label", "t10k-labels-idx1-ubyte.gz")
           ]
 
-datasetDir = "assets"
+dataSetDir = "assets"
+imgSize = 784
+
+generatePath :: String -> String
+generatePath p = dataSetDir ++ "/" ++ p
 
 download :: String -> IO ()
 download fileName = do
-    let filePath = datasetDir ++ "/" ++ fileName
+    let savePath = generatePath fileName
 
-    e <- doesFileExist filePath
+    e <- doesFileExist savePath
     unless e $ do
       putStrLn $ "Downloading " ++ fileName ++ " ..."
       res <- httpLBS =<< parseRequest (urlBase ++ "/" ++ fileName)
-      B.writeFile filePath (getResponseBody res)
+      BL.writeFile savePath (getResponseBody res)
       putStrLn "Done"
 
 downloadMnist :: [(String, String)] -> IO ()
@@ -36,6 +42,17 @@ downloadMnist [] = return ()
 downloadMnist (x:xs) = do
     download $ snd x
     downloadMnist xs
+
+loadImg :: String -> IO (Matrix R)
+loadImg fileName = do
+    let loadPath = generatePath fileName
+
+    putStrLn $ "Converting " ++ fileName ++ " to Matrix ..."
+    contents <- fmap GZ.decompress (BL.readFile loadPath)
+    putStrLn "Done"
+
+    let unpackData = map (read . show . fromEnum) $ BL.unpack $ BL.drop 16 contents
+    return $ matrix imgSize unpackData
 
 initMnist :: IO ()
 initMnist = downloadMnist keyFiles
--- src/test.hs
+++ src/test.hs
@@ -1,3 +1,3 @@
 import Mnist
 
-main = initMnist
+main = loadImg "train-images-idx3-ubyte.gz"
