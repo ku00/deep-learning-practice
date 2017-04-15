584adc66a247118edc47e8c7f0f153044d5126d1
ku00 <skentaro36@gmail.com>
2017-04-15 19:35:07 +0900

    MNIST の推論処理をするニューラルネットワークを実装
    TODO: ラベルデータは予め Vector にしておく

---
+++ src/NeuralnetMnist.hs
@@ -0,0 +1,29 @@
+import Control.Monad
+import Numeric.LinearAlgebra
+import ActivationFunction
+import Mnist
+import SampleWeight
+
+predict :: ([Matrix R], [Vector R]) -> Vector R -> Vector R
+predict ([w1,w2,w3],[b1,b2,b3]) x =
+    softMax' . (\x'' -> sumInput x'' w3 b3) . sigmoid . (\x' -> sumInput x' w2 b2) . sigmoid $ sumInput x w1 b1
+
+sumInput :: Vector R -> Matrix R -> Vector R -> Vector R
+sumInput x w b = (x <# w) + b
+
+maxIndexPredict :: ([Matrix R], [Vector R]) -> Matrix R -> Int -> Double
+maxIndexPredict sw i n = fromIntegral . maxIndex $ predict sw (i ! n)
+
+countAccuracy a n sw i l
+    | n <= 0    = a
+    | otherwise =
+      if maxIndexPredict sw i (n-1) == l ! (n-1)
+        then countAccuracy (a+1) (n-1) sw i l
+        else countAccuracy a (n-1) sw i l
+
+main = do
+    [_, (img, label)] <- loadMnist True
+    sw <- loadSW
+    let cnt = countAccuracy 0 (rows img) sw img (flatten label)
+
+    putStrLn $ "Accuracy: " ++ show (cnt / fromIntegral (rows img))
